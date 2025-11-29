937ca5e46689b882b6fd1486861612a94e9f2853
magmelblue <darfeandreshonor@gmail.com>
2025-11-29 12:31:10 +0800

    Delete HonorVargas/projectLang directory

--- HonorVargas/projectLang/My_app/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-def index(request):
-    projects = [
-        {"title": "Project 1", "desc": "Description 1", "image": "img/project1.jpg"},
-        {"title": "Project 2", "desc": "Description 2", "image": "img/project2.jpg"},
-        # Add more projects here as needed
-    ]
-    return render(request, "My_app/index.html", {"projects": projects})
